cc4ee14a451eca52c767576fbfda71ca4c5bb13b
taing28 <taing280703@gmail.com>
2025-06-18 15:10:07 +0700

    feat: product

---
+++ src/database/productRepository.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const {data: products} = require('./products.json');
+const {faker} = require('@faker-js/faker');
+
+function generateProducts(amount) {
+    const updatedProducts = []
+    for (let i = 1; i <= amount; i++) {
+        updatedProducts.push({
+            id: i,
+            name: faker.commerce.productName(),
+            price: faker.commerce.price({min: 1, max: 10000}),
+            description: faker.commerce.productDescription(),
+            product: faker.commerce.productAdjective(),
+            color: faker.color.human(),
+            createdAt: faker.date.past(),
+            image: faker.image.url()
+        })
+    }
+
+    fs.writeFileSync('./src/database/products.json', JSON.stringify({
+        data: updatedProducts
+    }))
+}
+
+function getAll(limit, sort) {
+    let filteredProducts = [...products];
+    if(limit) {
+        filteredProducts = filteredProducts.slice(0, limit);
+    }
+    if(sort) {
+        filteredProducts.sort((prev,next) => {
+            if(sort === 'asc') {
+                return new Date(prev.createdAt) - new Date(next.createdAt);
+            }
+            if(sort === 'desc') {
+                return new Date(next.createdAt) - new Date(prev.createdAt);
+            }
+        })
+    }
+    return filteredProducts;
+}
+
+function getOne(id) {
+    return products.find(p => p.id == id)
+}
+
+function add(data) {
+    data.createdAt = new Date();
+    const updateProducts = [...products, data];
+    fs.writeFileSync('./src/database/products.json', JSON.stringify({
+        data: updateProducts
+    }))
+    return data;
+}
+
+function update(id, data) {
+    const updateProducts = products.map(p => {
+        if (p.id == id) {
+            return {
+                ...p, 
+                ...data
+            }
+        }
+        return p;
+    })
+    
+    fs.writeFileSync('./src/database/products.json', JSON.stringify({
+        data: updateProducts
+    }))
+    return data;
+}
+
+function remove(id) {
+    const updateProducts = products.filter(p => p.id != id);
+    fs.writeFileSync('./src/database/products.json', JSON.stringify({
+        data: updateProducts
+    }))
+}
+
+function isExists(id) {
+    return products.some(p => p.id == id);
+}
+
+module.exports = {
+        generateProducts,
+        getAll,
+        getOne,
+        add,
+        update,
+        remove,
+        isExists
+    }
